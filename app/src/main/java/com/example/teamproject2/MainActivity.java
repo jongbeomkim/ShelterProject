34dfafcf10ea46b2b6bd68143e663c08715ef037
jongbeomkim <jbyqyq623@gmail.com>
2021-05-04 01:00:59 +0900

    메인액티비티 주석 달았음 이그노어에 파일 추가함

--- app/src/main/java/com/example/teamproject2/MainActivity.java
+++ app/src/main/java/com/example/teamproject2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.teamproject2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -9,15 +10,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mainCode = 11;
+
+    // activity_main.xml에 있는 버튼들을 묶어서 저장함
     private int[] btnId = new int[]{
             R.id.m_btn1, R.id.m_btn2, R.id.m_btn3,
             R.id.m_btn4, R.id.m_btn5, R.id.m_btn6
     };
+    // 버튼 6개를 저장할 배열을 선언함
     private Button[] btn = new Button[6];
 
+    // 프래그먼트의 자식들(Frag1 ~ Frag6)을 저장하기 위해 Fragment 형식으로 선언함
     private Fragment[] fragment = new Fragment[]{
             new Frag1(), new Frag2(), new Frag3(),
             new Frag4(), new Frag5(), new Frag6()
@@ -28,15 +35,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // btn 배열을 초기화
         for(int i=0; i<btn.length; i++){
             btn[i] = (Button) findViewById(btnId[i]);
         }
 
         for(int i=0; i<btn.length; i++){
             int finalI = i;
+            // btn[0~5]에 대한 onClickListener를 생성함
             btn[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    // 설명 필요
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment[finalI]).commit();
                     isBtnSelected(btn[finalI]);
                 }
@@ -56,12 +66,13 @@ public class MainActivity extends AppCompatActivity {
         switch(item.getItemId()){
             case R.id.edit:
                 intent = new Intent(this, EditActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, mainCode);
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    // 버튼을 6개 중 한 개 선택했을 경우 해당 버튼만 true로 하고 나머지는 false로 해주는 메서드
     public void isBtnSelected(Button tempButton){
         for(int i=0; i<btn.length; i++){
             if(btn[i] == tempButton){
