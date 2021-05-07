75259e452a817b27e9fb32c73e7a654e59953c12
DESKTOP-JPKL6DN\Leehj <dlgkrwns24@naver.com>
2021-05-07 20:47:10 +0900

    push_test한 것 삭제

--- app/src/main/java/com/example/teamproject2/MainActivity.java
+++ app/src/main/java/com/example/teamproject2/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //HJ_test
         // btn 배열을 초기화
         for(int i=0; i<btn.length; i++){
             btn[i] = (Button) findViewById(btnId[i]);
