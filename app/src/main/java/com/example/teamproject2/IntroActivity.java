cd206732a7629812b01204121b4bc2f161da94fe
dbswls627 <dbswls627@naver.com>
2021-05-12 18:21:33 +0900

    인트로  tweenanim

--- app/src/main/java/com/example/teamproject2/IntroActivity.java
+++ app/src/main/java/com/example/teamproject2/IntroActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -12,6 +15,9 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        ImageView img=(ImageView)findViewById(R.id.intro);
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.introanim);
+        img.startAnimation(anim);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
